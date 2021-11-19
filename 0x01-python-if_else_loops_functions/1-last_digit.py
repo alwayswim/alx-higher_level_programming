ff967bc3629a4a9fb75c449faeb9f3e2260a5a75
alwayswim <alwayswim@gmail.com>
2021-11-19 15:50:02 +0000

    last digit

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -6,13 +6,10 @@ if number >= 0:
     lastdigit = number % 10
 else:
     lastdigit = number % -10
-
 print("Last digit of {} is {}".format(number, lastdigit), end='')
-
-
 if lastdigit > 5:
-    print (" and is greater than 5")
+    print(" and is greater than 5")
 elif lastdigit == 0:
-    print (" and is zero")
+    print(" and is zero")
 else:
-    print (" and is less than 6 and not 0")
+    print(" and is less than 6 and not 0")
