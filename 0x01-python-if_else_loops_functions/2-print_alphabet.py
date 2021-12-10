42ece4e44409659381b60e3de07aaa481056b70b
alwayswim <alwayswim@gmail.com>
2021-12-10 05:28:45 +0000

    alphabet

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,6 +1,3 @@
 #!/usr/bin/python3
-alphabet = ""
-for number in range(97, 123):
-    alphabet = alphabet + chr(number)
-    number += 1
-print("{:s}".format(alphabet))
+for a in range(ord('a'), ord('z') + 1):
+    print("{:c}".format(a), end='')
